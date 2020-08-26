b557f76e268bd78571fbf0b076ce480f07a475cb
Harsh Singhal <harshsinghal726@gmail.com>
2020-08-26 23:36:03 +0530

    handled minor issues

--- setup.py
+++ setup.py
@@ -5,19 +5,20 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='amazon_product_review_scraper',
-    version='1.0',
+    packages=find_packages(),
+    version='0.1',
     description='Python package to scrape product review data from amazon',
     long_description_content_type="text/markdown",
     long_description=README,
     license='MIT',
-    packages=find_packages(),
+    keywords=["Scraper", "Amazon", "Reviews"],
     author='Harsh Singhal',
     author_email='harshsinghal726@gmail.com',
     python_requires='>=3.6',
 )
 
 install_requires = [
-    'bs4',
+    'beautifulsoup4',
     'tqdm',
     'fake_useragent'
 ]
